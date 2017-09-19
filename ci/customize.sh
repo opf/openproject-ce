cd35cd3359dea40ec23dd9bce9eb016f7842f78a
Oliver Günther <mail@oliverguenther.de>
2017-09-19 13:35:57 +0200

    Remove docker customization in CE

--- ci/customize.sh
+++ ci/customize.sh
@@ -4,14 +4,9 @@ pushd ${WORKSPACE_DIR}
 # Copy common travis configuration
 cp ${CUSTOMIZATION_DIR}/.travis.yml .
 
-# Override docker
-cp -r ${CUSTOMIZATION_DIR}/docker .
-cp ${CUSTOMIZATION_DIR}/Dockerfile .
-cp ${CUSTOMIZATION_DIR}/Dockerfile.public .
-
 # Copy custom gemfiles
 cp ${CUSTOMIZATION_DIR}/Gemfile.plugins .
 
-git add -f .travis.yml Gemfile.plugins Dockerfile Dockerfile.public docker
+git add -f .travis.yml Gemfile.plugins
 
 popd
